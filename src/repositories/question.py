88209c08210b6a80a2fa67ee06d80aef311c6668
abrigo132 <garipotter97@gmail.com>
2025-08-28 15:06:13 +0500

    create question repo

---
+++ src/repositories/question.py
@@ -0,0 +1,30 @@
+from sqlalchemy import select, ScalarResult, Sequence
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Type
+
+from core.models import Question
+from core.schemas import QuestionCreateRequest
+
+
+class QuestionRepository:
+
+    def __init__(self, session: AsyncSession) -> None:
+        self.session: AsyncSession = session
+        self.model: Type[Question] = Question
+
+    async def add(self, question_creds: QuestionCreateRequest) -> Question:
+        question = self.model(**question_creds.model_dump())
+        self.session.add(question)
+        await self.session.flush()
+        return question
+
+    async def get(self) -> Sequence[Question]:
+        stmt = select(self.model)
+        questions: ScalarResult[Question] = await self.session.scalars(stmt)
+        return questions.all()
+
+    async def get_by_id(self, id: int) -> Question:
+        pass
+
+    async def delete(self, id: int):
+        pass
