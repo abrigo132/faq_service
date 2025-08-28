342c1c3954801d5f49cd010ab05391097726abb4
abrigo132 <garipotter97@gmail.com>
2025-08-28 15:03:33 +0500

    include question router

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
 from core import settings
+from .question import router as question_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
+router.include_router(router=question_router)
