45755b98bf3764664a4f124028c4cc268dfa8d8d
abrigo132 <garipotter97@gmail.com>
2025-08-29 19:01:13 +0500

    create answer pydantic schemas

---
+++ src/core/schemas/answer.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field, field_validator
+
+
+class AnswerBase(BaseModel):
+    """
+    Answer Base pydantic model
+    """
+
+    question_id: int
+    user_id: str
+    text: str = Field(
+        min_length=1,
+        description="Текст ответа на вопрос не может быть пустым, или состоять из одних пробелов",
+    )
+    created_at: datetime
+
+    @field_validator("text")
+    @classmethod
+    def validate_text_not_empty(cls, v):
+        if not v.strip():
+            raise ValueError(
+                "Текст ответа не может быть пустым или состоять только из пробелов"
+            )
+        return v
+
+
+class AnswerByIdRequest(AnswerBase):
+    """
+    Pydantic scheme for request answer by id
+    """
+
+    id: int
+
+
+class AnswerCreateRequest(AnswerBase):
+    """
+    Pydantic scheme for create answer
+    """
+
+    pass
+
+
+class AnswerCreateResponse(AnswerBase):
+    id: int
