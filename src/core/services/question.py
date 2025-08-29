c5be5a3eadd234b0c78a1082f7f43a5e2f0e5f03
abrigo132 <garipotter97@gmail.com>
2025-08-29 19:03:02 +0500

    add method get question with answers

--- src/core/services/question.py
+++ src/core/services/question.py
@@ -31,5 +31,12 @@ class QuestionService:
         await self.session.commit()
         return {"message": f"Ответ с ID {id} удален"}
 
-    async def get_question_with_answers(self, id: int):
-        pass
+    async def get_question_with_answers(self, id: int) -> Question | HTTPException:
+        question: Question | None = await self.question_repo.get_by_id(id=id)
+        if question is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Вопрос с {id=} не найден",
+            )
+
+        return question
