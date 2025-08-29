e51f2d102328f7f028027217c98bd9e36abb2510
abrigo132 <garipotter97@gmail.com>
2025-08-29 19:01:55 +0500

    add cascade delete

--- src/core/models/question.py
+++ src/core/models/question.py
@@ -9,5 +9,17 @@ from core.models.mixins import IdIntPkMixin
 class Question(Base, IdIntPkMixin):
     text: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(), onupdate=func.now(), nullable=False
+        server_default=func.now(), nullable=False
+    )
+    answers: Mapped[List["Answer"]] = relationship(
+        "Answer", back_populates="question", cascade="all, delete"
+    )
+
+
+class Answer(Base, IdIntPkMixin):
+    user_id: Mapped[str] = mapped_column(nullable=False)
+    text: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        nullable=False,
+        server_default=func.now(),
     )
