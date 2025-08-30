f4a37741d1f95f6c4065ee451b52b13e7539df56
abrigo132 <garipotter97@gmail.com>
2025-08-30 09:47:50 +0500

    add configure logging + log request for export

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,8 +2,11 @@ __all__ = (
     "settings",
     "db_helper",
     "lifespan",
+    "configure_logging",
+    "log_requests",
 )
 
-from .settings import settings
+from .settings import settings, configure_logging
 from .db_helper import db_helper
 from .lifespan import lifespan
+from .middleware import log_requests
