5f211af172eb31b5e301b6ea6f816b8da7f4216b
abrigo132 <garipotter97@gmail.com>
2025-08-30 10:47:23 +0500

    add exceptions

--- src/repositories/answer.py
+++ src/repositories/answer.py
@@ -1,9 +1,15 @@
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Type
 from sqlalchemy import select
+import logging
 
+from core import DuplicateEntryException, ForeignKeyViolationException
 from core.models import Answer
 from core.schemas import AnswerCreateRequest
+
+
+logger = logging.getLogger(__name__)
 
 
 class AnswerRepository:
@@ -13,10 +19,33 @@ class AnswerRepository:
         self.model: Type[Answer] = Answer
 
     async def add(self, answer_creds: AnswerCreateRequest) -> Answer:
-        answer = self.model(**answer_creds.model_dump())
-        self.session.add(answer)
-        await self.session.flush()
-        return answer
+        try:
+            answer = self.model(**answer_creds.model_dump())
+            self.session.add(answer)
+            await self.session.flush()
+            return answer
+        except IntegrityError as e:
+            logger.error(
+                "Не удалось создать вопрос с текстом: %r",
+                answer_creds.text,
+                exc_info=True,
+            )
+            await self.session.rollback()
+            error_msg = str(e.orig)
+            if "unique" in error_msg.lower():
+                logger.error(
+                    "Не удалось создать вопрос с текстом: %r - дубликат",
+                    answer_creds.text,
+                    exc_info=True,
+                )
+                raise DuplicateEntryException("Вопрос с таким текстом уже существует")
+            else:
+                logger.error(
+                    "Не удалось создать вопрос с текстом: %r - ошибка целостности",
+                    answer_creds.text,
+                    exc_info=True,
+                )
+                raise ForeignKeyViolationException("Ошибка целостности данных")
 
     async def get_by_id(self, answer_id: int) -> Answer | None:
         stmt = select(self.model).where(self.model.id == answer_id)
