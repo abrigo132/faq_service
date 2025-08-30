8a889726033de64811be390f70a278d6db379205
abrigo132 <garipotter97@gmail.com>
2025-08-30 10:47:00 +0500

    add custom exception for export

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -4,9 +4,19 @@ __all__ = (
     "lifespan",
     "configure_logging",
     "log_requests",
+    "DuplicateEntryException",
+    "ForeignKeyViolationException",
+    "NotFoundException",
+    "RepositoryException",
 )
 
 from .settings import settings, configure_logging
 from .db_helper import db_helper
 from .lifespan import lifespan
 from .middleware import log_requests
+from .excpetions import (
+    DuplicateEntryException,
+    NotFoundException,
+    ForeignKeyViolationException,
+    RepositoryException,
+)
