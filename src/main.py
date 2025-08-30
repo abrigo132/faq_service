9e5c0661ece44fbc5b752348bc4696f54f4e15df
abrigo132 <garipotter97@gmail.com>
2025-08-30 09:48:41 +0500

    add fastapi middleware

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from uvicorn import run
 
-from core import lifespan, settings
+from core import lifespan, settings, log_requests
 from api import router as api_router
 
 app = FastAPI(default_response_class=ORJSONResponse, lifespan=lifespan)
 app.include_router(router=api_router)
 
+app.middleware("http")(log_requests)
 
 if __name__ == "__main__":
     run(
