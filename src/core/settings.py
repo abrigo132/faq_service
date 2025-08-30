6cf5a7b9c351f56535ce5cc671c0cbdacca115ab
abrigo132 <garipotter97@gmail.com>
2025-08-30 09:50:10 +0500

    create func configure logging

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
+import logging
+
+
+def configure_logging(level=logging.INFO):
+    logging.basicConfig(
+        level=level,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        format="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
+    )
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
